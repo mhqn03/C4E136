5d4f43b47065c54f2962c46926b915b456d3421f
Minh Quân <mquanv12@gmail.com>
2023-02-26 02:45:26 +0700

    first

---
+++ bmi.js
@@ -0,0 +1,17 @@
+function send() {
+    let h = document.getElementById('height').value;
+    let w = document.getElementById('weight').value;
+    let x = w/(h*h);
+    document.getElementById('submit').innerHTML = x.toFixed(2);
+    document.write('chỉ số bmi của bạn là: ',x.toFixed(2),', và bạn: ');
+    if(x>=30){
+        document.write('béo đét');
+    } else if(x>=25){
+        document.write('thừa cân');
+    } else if(x>=18.5){
+        document.write('bình thường');
+    } else {
+        document.write('nghiện');
+    }
+    
+}
