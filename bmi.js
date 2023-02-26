a0cf0a87787bb396d3a5a5e2a0a7c20869720fe8
Minh Quân <mquanv12@gmail.com>
2023-02-26 16:24:49 +0700

    first

--- bmi.js
+++ bmi.js
@@ -3,15 +3,19 @@ function send() {
     let w = document.getElementById('weight').value;
     let x = w/(h*h);
     document.getElementById('submit').innerHTML = x.toFixed(2);
-    document.write('chỉ số bmi của bạn là: ',x.toFixed(2),', và bạn: ');
-    if(x>=30){
-        document.write('béo đét');
-    } else if(x>=25){
-        document.write('thừa cân');
-    } else if(x>=18.5){
-        document.write('bình thường');
-    } else {
-        document.write('nghiện');
+    {
+        document.write('chỉ số bmi của bạn là: ',x.toFixed(2),', và bạn: ');
+        if(x>=30){
+            document.write('béo đét');
+        } else if(x>=25){
+            document.write('thừa cân');
+        } else if(x>=18.5){
+            document.write('bình thường');
+        } else {
+            document.write('nghiện');
+        }
     }
-    return;
 }
+// function acp() {
+//     document.getElementById('return').p
+// }
